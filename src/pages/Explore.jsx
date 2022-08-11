a518aa86112c61cfec288d4b92765e84821c1247
Ale Emmanuel Rabelo Guedes <rabeloguedes@proton.me>
2022-08-11 12:33:53 -0300

    refactory: Centraliza os botões de explore na tela.

--- src/pages/Explore.jsx
+++ src/pages/Explore.jsx
@@ -3,26 +3,33 @@ import { useHistory } from 'react-router-dom';
 import Header from '../components/Header';
 import Button from '../components/Button';
 import BottomMenu from '../components/BottomMenu';
+import '../styles/Explore.css';
 
 export default function Explore() {
   const history = useHistory();
   return (
-    <div>
+    <section
+      className="exploreSection"
+    >
       <Header
         title="Explore"
         showSearchIcon={ false }
       />
-      <Button
-        dataTestIdButton="explore-foods"
-        name="Explore Foods"
-        onClick={ () => history.push('/explore/foods') }
-      />
-      <Button
-        dataTestIdButton="explore-drinks"
-        name="Explore Drinks"
-        onClick={ () => history.push('/explore/drinks') }
-      />
+      <div
+        className="exploreBtns"
+      >
+        <Button
+          dataTestIdButton="explore-foods"
+          name="Explore Foods"
+          onClick={ () => history.push('/explore/foods') }
+        />
+        <Button
+          dataTestIdButton="explore-drinks"
+          name="Explore Drinks"
+          onClick={ () => history.push('/explore/drinks') }
+        />
+      </div>
       <BottomMenu />
-    </div>
+    </section>
   );
 }
