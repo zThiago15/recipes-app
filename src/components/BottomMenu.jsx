768047e753d5e68cfc14e40b508646d7fcf429dd
Ale Emmanuel Rabelo Guedes <rabeloguedes@proton.me>
2022-08-11 12:16:50 -0300

    refactory: Muda a estrutura jsx do BottomMenu e a forma de estiliza-lo.

--- src/components/BottomMenu.jsx
+++ src/components/BottomMenu.jsx
@@ -10,38 +10,59 @@ export default function BottomMenu() {
 
   const path = window.location.href;
 
+  const ACTIVE_PAGE = 'pageBtn active';
+
+  const ACTIVE_EXPLORE = 'exploreBtn active';
+
+  const checkExplorePath = () => {
+    if (path.includes('explore') && (path.includes('food') || path.includes('drink'))) {
+      return ACTIVE_EXPLORE;
+    }
+    return ACTIVE_PAGE;
+  };
+
   return (
     <footer
       className="footer"
       data-testid="footer"
     >
-      <div className={ path.includes('drinks') ? 'btnSelected' : 'btnStandard' }>
-        <button
-          type="button"
-          onClick={ () => history.push('/drinks') }
+      <div className="footerBtns">
+        <div className={ path.includes('drinks') ? ACTIVE_PAGE : 'pageBtn' }>
+          <button
+            type="button"
+            onClick={ () => history.push('/drinks') }
+          >
+            <img
+              src={ drinkIcon }
+              alt="Ícone de bebida"
+              data-testid="drinks-bottom-btn"
+            />
+          </button>
+        </div>
+        <div
+          className={
+            path.includes('explore') ? checkExplorePath() : 'pageBtn'
+          }
         >
-          <img src={ drinkIcon } alt="Ícone de bebida" data-testid="drinks-bottom-btn" />
-        </button>
-      </div>
-      <div className={ path.includes('explore') ? 'btnSelected' : 'btnStandard' }>
-        <button
-          type="button"
-          onClick={ () => history.push('/explore') }
-        >
-          <img
-            src={ exploreIcon }
-            alt="Ícone de exploração"
-            data-testid="explore-bottom-btn"
-          />
-        </button>
-      </div>
-      <div className={ path.includes('foods') ? 'btnSelected' : 'btnStandard' }>
-        <button
-          type="button"
-          onClick={ () => history.push('/foods') }
-        >
-          <img src={ mealIcon } alt="Ícone de comida" data-testid="food-bottom-btn" />
-        </button>
+          <button
+            type="button"
+            onClick={ () => history.push('/explore') }
+          >
+            <img
+              src={ exploreIcon }
+              alt="Ícone de exploração"
+              data-testid="explore-bottom-btn"
+            />
+          </button>
+        </div>
+        <div className={ path.includes('foods') ? ACTIVE_PAGE : 'pageBtn' }>
+          <button
+            type="button"
+            onClick={ () => history.push('/foods') }
+          >
+            <img src={ mealIcon } alt="Ícone de comida" data-testid="food-bottom-btn" />
+          </button>
+        </div>
       </div>
     </footer>
   );
